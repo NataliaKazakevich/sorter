36c8a5073929c03ac4be43bf9ae9a229b5adb66f
Natalia Kazakevich <Natallia_Kazakevich@mail.ru>
2018-09-23 23:49:51 +0300

    sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,144 @@
-class Sorter {
+/*class Sorter {
   constructor() {
-    // your implementation
+    this.array = new Array(); 
+  }
+  add(element) {
+   this.array.push(element);
+  }
+  at(index) {
+    return this.array[index];
+  }
+  get length() {
+    return this.array.length;
+  }
+  toArray() {
+    return this.array;
+  }
+  sort(indices) {
+    // sortirovka indicies
+    var len = indices.length;
+    for (var i = 0; i < len-1; i++){
+      for (var j = i + 1; j < len; j++) {
+        if (indices[i] > indices[j]) {
+          let bolsh = indices[i];
+          indices[i] = indices[j];
+          indices[j] = bolsh;
+        }
+      }
+    }
+  if ( this.compareFunction !== undefined) {
+    var testFunction = this.compareFunction(1, 0);
+      var isReverse = false;
+      if (testFunction === -1) {
+        isReverse = true;
+      }
+    var len = indices.length;
+    for (var i = 0 ; i < len - 1; i++) {
+      for(var j = i + 1; j < len; j++) {
+        if ((this.compareFunction(this.array[indices[i]], 
+            this.array[indices[j]]) > 0 && !isReverse) || 
+            (this.compareFunction(this.array[indices[i]], 
+            this.array[indices[j]]) > 0 && isReverse)) {
+          let bolsh = this.array[indices[j]];
+          this.array[indices[i]] = this.array[indices[j]];
+          this.array[indices[j]] = bolsh;
+        }
+        else{
+          continue;
+        }
+      }
+    }
+  } 
+  else {
+    var len = indices.length;
+    for (var i = 0; i < len - 1; i++) {
+      for(var j = i + 1; j < len; j++) {
+        if ( this.array[indices[i]] <= this.array[indices[j]]) {
+          continue;
+        } 
+        else {
+          let bolsh = this.array[indices[j]];
+          this.array[indices[j]] = this.array[indices[i]];
+          this.array[indices[i]] = bolsh;
+        }
+      }
+    }
+  }
+  setComparator(compareFunction)
+    this.compareFunction = compareFunction;
+  }    
+  }
+module.exports = Sorter;
+*/
+class Sorter {
+
+
+  constructor() {
+    this.array = new Array();
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    var sortIndices = false;
+    for (var i = 0; i < indices.length-1; i++) {
+      for (var j = i + 1; j < indices.length; j++) {
+        if (indices[i] > indices[j]) {
+          let temp = indices[i];
+          indices[i] = indices[j];
+          indices[j] = temp;
+        }
+      }
+    }
+    if (this.compareFunction !== undefined) { 
+      var testFunction = this.compareFunction(1, 0);
+      var isReverse = false;
+      if (testFunction === -1) {
+        isReverse = true;
+      }
+      for (var i = 0; i < indices.length - 1; i++) {
+        for (var j = i + 1; j < indices.length; j++) {
+          if ((this.compareFunction(this.array[indices[i]], this.array[indices[j]]) > 0 && !isReverse) ||
+              (this.compareFunction(this.array[indices[i]], this.array[indices[j]]) > 0 && isReverse)) {
+            let temp = this.array[indices[j]];
+            this.array[indices[j]] = this.array[indices[i]];
+            this.array[indices[i]] = temp;
+          }else{
+            continue;
+          }
+        }
+      }
+    } else {
+      for (var i = 0; i < indices.length-1; i++) {
+        for (var j = i + 1; j < indices.length; j++) {
+          if (this.array[indices[i]] <= this.array[indices[j]]) {
+            continue;
+          }else{
+            let temp = this.array[indices[j]];
+            this.array[indices[j]] = this.array[indices[i]];
+            this.array[indices[i]] = temp;
+          }
+        }
+      }
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
